66f4390483daec8a9ec05439a2aa6bb77cd56b68
Kunal Sharma <kunal1.sharma1@mobileprogramming.com>
2021-02-13 13:03:27 +0530

    Added Dependency
    1.  Added Gradle Dependency
    2. Added Const Class

---
+++ MVVMStructure/app/src/main/java/com/jobapply/myapplication/utils/Consts.kt
@@ -0,0 +1,9 @@
+package com.jobapply.myapplication.utils
+
+object Consts {
+    const val NEW_API_KEY = "db010d13d4654541a0498ce7abdabc14"
+    const val   BASE_URL = "http://newsapi.org/"
+    // http://newsapi.org/v2/everything?q=tesla&from=2021-01-13&sortBy=publishedAt&apiKey=db010d13d4654541a0498ce7abdabc14
+    // http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=db010d13d4654541a0498ce7abdabc14
+    // 
+}
